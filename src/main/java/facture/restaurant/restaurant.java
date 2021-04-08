6f155facc41e5b35dce5731b18b8e1d71531e347
ValG94 <79853605+ValG94@users.noreply.github.com>
2021-04-08 13:44:50 +0200

    dernieres modifications

--- src/main/java/facture/restaurant/restaurant.java
+++ src/main/java/facture/restaurant/restaurant.java
@@ -3,15 +3,13 @@ package facture.restaurant;
 import models.Facture;
 import models.Plats;
 import models.Serveur;
-
 import java.sql.*;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class restaurant {
 
     private static Connection connection;
+
 
     public static void main(String[] menu) {
 
@@ -21,68 +19,51 @@ public class restaurant {
         String user = "postgres";
         String password = "postgres";
         Scanner sc = new Scanner(System.in);
-
+        boolean sortir = false;
 
         try {
             Connection connection = DriverManager.getConnection(url, user, password);
-            Plats.getPlats(connection);
-            Serveur.getServeur(connection);
-            Facture.getFacture(connection);
+            while (sortir==false){
 
-            //Utilisation d'un Switch pour afficher le menu
-            System.out.println("Afficher le menu des plats :");
-            int m = sc.nextInt();
-            //pour pouvoir appeler le menu de mes plats
-            switch (m) {
-                case 1: {
-                    Plats.getPlats(connection);
-                    break;
+
+
+                System.out.println("__________________Menu du Programme_______________");
+                System.out.println("1. Menu des plats" + '\n' + "2. Les serveurs" + '\n' + "3. Les factures"+ '\n' + "4. Exit");
+                System.out.println("__________________________________________________");
+                System.out.print("Faites votre choix : " );
+                int m = sc.nextInt();
+                sc.nextLine();
+
+                //Utilisation d'un Switch afin de pouvoir appeler le menu de mes plats
+                switch (m) {
+                    case 1: {
+                        System.out.println("Menu");
+                        Plats.getPlats(connection);
+
+                        break;}
+                    case 2: {
+                        System.out.println("Les serveurs");
+                        Serveur.getServeur(connection);
+
+                        break;
+                    }
+                    case 3:{
+                        System.out.println("Les factures");
+                        Facture.getFacture(connection);
+                        break;
+                    }
+                    case 4:{
+                        System.out.println("Exit");
+                        sortir = true;
+                        break;
+                    }
+                    default:
+                        System.out.println("Erreur du menu");
                 }
-
-                default:
-                    System.out.println("Erreur du menu");
             }
-
-            //Ajouter un nouveau plat et son prix
-            System.out.println("Saisir un nouveau plat");
-            String newNomPlat = sc.nextLine();
-            System.out.print("Quel est le prix du plat ? ");
-            double prixHT = sc.nextDouble();
-
-            Plats nouveauPlat = new Plats(newNomPlat, prixHT);
-            nouveauPlat.newPlat(connection);
-
-            // Ajouter un nouveau Serveur
-            System.out.println("Saisir un nouveau Serveur");
-            int newIdServeur = sc.nextInt();
-            System.out.println("Quel est le prenom du nouveau serveur ?");
-            String prenom = sc.nextLine();
-            System.out.println("Quel est le Nom du nouveau serveur ? ");
-            String nom = sc.nextLine();
-
-            Serveur nouveauServeur = new Serveur(newIdServeur, prenom, nom);
-            nouveauServeur.nouveauServeur(connection);
-
-            // Ajouter une nouvelle facture
-            System.out.println("Nouvelle facture");
-            int newIdFacture = sc.nextInt();
-            System.out.println("Quel est le numéro de la table ?");
-            int newTable = sc.nextInt();
-            System.out.println("Quel est le prenom du serveur ?");
-            int IdServeur = sc.nextInt();
-            System.out.println("Quels sont les plats commandés ?");
-            //List<Plats> platCommande=new ArrayList<Plats>();// A TERMINER
-
-            Facture newFacture = new Facture(newIdFacture, newTable, IdServeur);
-            newFacture.newFacture(connection);
-
-
-            connection.close();
 
         } catch (SQLException exception) {
             exception.printStackTrace();
         }
-
-
     }
 }
--- src/main/java/models/Facture.java
+++ src/main/java/models/Facture.java
@@ -8,36 +8,40 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Facture {
-private int idFacture;
-private int numTable;
-private int idServeur;
-//private List<Plats> menu;
+    private int idFacture;
+    private int numTable;
+    private int idServeur;
+    private String nomPlat;
+    private Double prixHT;
 
-//Création de mon objet Facture
-    public Facture( int numTable, int idServeur, List<Plats> menu) {
-    this.numTable = numTable;
-    this.idServeur = idServeur;
-    //this.menu = menu;
+    //Création de mon objet Facture
+    public Facture(int numTable, int idServeur, String nomPlat, Double prixHT) {
+        this.numTable = numTable;
+        this.idServeur = idServeur;
+        this.nomPlat = nomPlat;
+        this.prixHT = prixHT;
     }
 
-    public Facture(int newIdFacture, int newTable, int newIdServeur) {
+    public Facture(int newIdFacture, int newTable, int newIdServeur, String newNomTable, Double newPrixHT) {
         this.idFacture = newIdFacture;
         this.numTable = newTable;
         this.idServeur = newIdServeur;
+        this.nomPlat = newNomTable;
+        this.prixHT = newPrixHT;
 
     }
 
 
     @Override
     public String toString() {
-        return "Facture{" + idFacture + ", Table n°=" + numTable + ", Serveur=" + idServeur; //+ "\t"+ "Plat commandé" + menu;
+        return "Facture" + idFacture + ", Table n°=" + numTable + ", Serveur=" + idServeur + ", Plat commandé : "  + nomPlat + ", Prix HT : " + prixHT;
     }
 
 
     // Méthode pour enregistrer une nouvelle facture
     public void newFacture(Connection connection) throws SQLException {
         Statement ordreSQL = connection.createStatement();
-        ordreSQL.execute("INSERT INTO Facture(id_facture, id_table_restaurant, id_serveur) VALUES ('" + idFacture + "'\t'" + numTable + "'\t'"+ idServeur+"')");
+        ordreSQL.execute("INSERT INTO Facture(id_facture, id_table_restaurant, id_serveur) VALUES ('" + idFacture + "'\t'" + numTable + "'\t'" + idServeur + "')");
         ordreSQL.close();
     }
 
@@ -45,14 +49,19 @@ private int idServeur;
     public static List<Facture> getFacture(Connection connection) throws SQLException {
         //Permet d'afficher la liste des factures
         Statement ordreSQL = connection.createStatement();
-        ResultSet resultats = ordreSQL.executeQuery("SELECT id_facture, id_table_restaurant, id_serveur from Facture ");
+        ResultSet resultats = ordreSQL.executeQuery("SELECT ligne_facture.id_facture, \"Facture\".id_table_restaurant, \"Facture\".id_serveur, plats.nom_plat, plats.\"prixHT\" " +
+                "from \"Facture\" " +
+                "inner join ligne_facture on \"Facture\".id_facture = ligne_facture.id_facture " +
+                "inner join plats on ligne_facture.id_plat = plats.id_plat ");
 
         List<Facture> factureList = new ArrayList<>();
 
         while (resultats.next()) {
             Facture dbFacture = new Facture(resultats.getInt("id_facture"),
                     resultats.getInt("id_table_restaurant"),
-                    resultats.getInt("id_serveur"));
+                    resultats.getInt("id_serveur"),
+                    resultats.getString("nom_plat"),
+                    resultats.getDouble("prixHT"));
 
             factureList.add(dbFacture);
 
@@ -63,4 +72,6 @@ private int idServeur;
         return factureList;
     }
 
-}
+
+    }
+
